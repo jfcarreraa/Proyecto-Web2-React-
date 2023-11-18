6171899ce462a585c94750bfb52334365a268c02
dilana_sancho <dilana.sancho@factork-software.com>
2023-11-17 19:42:01 -0600

    archivo firebase

---
+++ web2/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBuxjUdFYr7eybg8BrYvQCnvULvYHdzR0w",
+  authDomain: "web-app-fc023.firebaseapp.com",
+  projectId: "web-app-fc023",
+  storageBucket: "web-app-fc023.appspot.com",
+  messagingSenderId: "1073228764718",
+  appId: "1:1073228764718:web:9b88c7755b65c1d2b571bf"
+};
+// no te creo 
+//blablabla
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
